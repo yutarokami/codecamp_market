51db9f41cfe3b5a30442ec3a2fe3d99d65793a39
EC2 Default User <ec2-user@ip-172-31-87-64.ec2.internal>
2022-11-30 01:00:30 +0900

    プロジェクトの初期設定

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string
      */
-    protected $proxies;
+    protected $proxies='*'; // すべてのプロクシを信頼する
 
     /**
      * The headers that should be used to detect proxies.
