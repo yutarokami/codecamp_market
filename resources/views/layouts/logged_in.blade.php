010da7322de82c8da3c2c1cd3b8562affc78343b
EC2 Default User <ec2-user@ip-172-31-87-64.ec2.internal>
2022-12-01 01:00:02 +0900

    layoutsフォルダのbladeファイルの中身作成

---
+++ resources/views/layouts/logged_in.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.default')
+ 
+@section('header')
+<header>
+    <ul class="header_nav">
+        <li>
+          <a href="/">
+            トップページ
+          </a>
+        </li>
+        <li>
+          <a href="{{ route('likes.index') }}">
+            お気に入り一覧
+          </a>
+        </li>
+        <li>
+          <a href="{{ route('users.exhibitions') }}">
+            出品商品一覧
+          </a>
+        </li>
+        <li>
+          <a href="{{ route('users.show') }}">
+            プロフィール
+          </a>
+        </li>
+        <li>ログアウト</li>
+    </ul>
+</header>
+@endsection
