a8342305184940876a19fd3b8bf70a84e2a40124
EC2 Default User <ec2-user@ip-172-31-87-64.ec2.internal>
2022-12-01 16:22:43 +0900

    新規出品ページを作成

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -4,4 +4,49 @@
  
 @section('content')
   <h1>{{ $title }}</h1>
+  <h2>商品追加フォーム</h2>
+  <form method='post' action="{{ route('items.store') }}">
+      @csrf
+      <label>
+          <div>
+            商品名：
+          </div>
+          <div>
+            <input type='text' name='name'>
+          </div>
+      </label>
+      <label>
+          <div>
+            商品説明：
+          </div>
+          <div>
+            <textarea name='description' rows='10' cols='50'>
+            </textarea>
+          </div>
+      </label>
+      <label>
+          <div>
+            価格：
+          </div>
+          <div>
+            <input type='number' name='price'>
+          </div>
+      </label>
+      <label>
+          <div>
+            カテゴリー：
+          </div>
+          <div>
+            <select name='category'>
+            </select>
+          </div>
+      </label>
+      <label>
+          <div>
+            画像を選択：
+            <input type='file' name='image'>
+          </div>
+      </label>
+      <input type='submit' value='出品'>
+  </form>
 @endsection
