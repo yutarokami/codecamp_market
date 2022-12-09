1e34d20b25cfd369a9ecea3ce489c9a59e0fcc82
EC2 Default User <ec2-user@ip-172-31-87-64.ec2.internal>
2022-12-09 20:39:45 +0900

    UserControllerの実装

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,17 +6,5 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    // プロフィール詳細
-    public function show() {
-        return view('users.show', [
-            'title' => 'プロフィール',
-        ]);
-    }
     
-    // 出品商品一覧
-    public function exhibitions() {
-        return view('users.exhibitions', [
-            'title' => '出品商品一覧',
-        ]);
-    }
 }
