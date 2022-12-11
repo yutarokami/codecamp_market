c2e34e89ae386a40cc650a06835d107afe3c02ff
EC2 Default User <ec2-user@ip-172-31-87-64.ec2.internal>
2022-12-11 10:35:12 +0900

    プロフィール画面の作成

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Item;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function show($id) {
         $user = User::find($id);
+        $item_amount = Item::where('user_id', $id)->count();
         return view('users.show', [
             'title' => 'プロフィール',
             'user' => $user,
+            'item_amount' => $item_amount,
         ]);
     }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,5 +4,26 @@
  
 @section('content')
   <h1>{{ $title }}</h1>
-  <p>{{ $user->name }}</p>
+  
+  <div>
+    @if( $user->image!=='')
+      <img src="{{ asset('storage/' . $user->image) }}">
+    @else
+      <img src="{{ asset('images/no_image.png') }}">
+    @endif
+    <a href="{{ route('profile.edit_image') }}">画像を変更</a>
+  </div>
+  
+  <div>
+    <p>{{ $user->name }} さん</p>
+    <a href="{{ route('profile.edit') }}">プロフィールを編集</a>
+  </div>
+  
+  <p>{{ $user->profile }}</p>
+  
+  <p>出品数: {{ $item_amount }}</p>
+  <h2>購入履歴</h2>
+  <div>
+      <p></p>
+  </div>
 @endsection
